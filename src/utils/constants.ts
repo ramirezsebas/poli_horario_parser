a4b3b905178d6b1c4f1ce263845e87d170f285bb
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2023-07-17 10:47:58 -0400

    feat: se agrega constantes iniciales sobre el excel

---
+++ src/utils/constants.ts
@@ -0,0 +1,20 @@
+export const sheets = [
+  "IAE",
+  "ICM",
+  "IEK",
+  "IEL",
+  "IEN",
+  "IIN",
+  "IMK",
+  "ISP",
+  "LCA",
+  "LCI",
+  "LCIk",
+  "LEL",
+  "LGH",
+  "TSE",
+];
+
+export const initialColumn = "A";
+
+export const initialRow = 11;
