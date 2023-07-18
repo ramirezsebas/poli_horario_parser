7a9999544e282791bea87387e7a2c6a087316def
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2023-07-18 12:51:27 -0400

    refactor: quitamos async del retorno de carreras

--- src/app/api/carreras/route.ts
+++ src/app/api/carreras/route.ts
@@ -2,7 +2,7 @@ import { sheets } from "@/utils/constants";
 import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
-export async function GET(request: Request) {
+export function GET(request: Request) {
   return NextResponse.json(
     sheets.map((sheet) => {
       return {
